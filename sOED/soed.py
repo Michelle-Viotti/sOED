9fca20ffa24c825da26da202853645d6031a9e41
Michelle-Viotti <95856413+Michelle-Viotti@users.noreply.github.com>
2022-04-21 11:34:18 +0100

    correct call to self.prior_rvs

--- sOED/soed.py
+++ sOED/soed.py
@@ -734,7 +734,7 @@ class SOED(object):
             return 0
             
         else:
-            thetas_sPCE = prior_rvs(L+1) # should work as uses a lambda trick
+            thetas_sPCE = self.prior_rvs(L+1) # should work as uses a lambda trick
             post_thetas = post_pdf(thetas_sPCE, stage=0, d_hist=d_hist, 
                                   y_hist=y_hist, xp_hist=xp_hist, 
                                   include_prior=False) # well...
